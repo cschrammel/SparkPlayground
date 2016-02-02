aa37ddb8f1d279ba361810cdf9146fe4a95b984a
Carl Schrammel <carl.schrammel@gmail.com>
2016-02-02 15:50:04 +0000

    new

---
+++ SparkClrPoc/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Spark.CSharp.Core;
+using Microsoft.Spark.CSharp.Sql;
+
+namespace SparkClrPoc
+{
+    class Program
+    {
+        private static SqlContext sqlContext;
+        internal static SparkContext SparkContext;
+
+        static void Main(string[] args)
+        {
+            SparkContext = CreateSparkContext();
+            SparkContext.SetCheckpointDir(Path.GetTempPath());
+
+            var crimeDataFrame = GetSqlContext().Read().Json(@"data\SacramentocrimeJanuary2006.csv");
+
+            SparkContext.Stop();
+        }
+
+        // Creates and returns a context
+        private static SparkContext CreateSparkContext()
+        {
+            var conf = new SparkConf();
+            return new SparkContext(conf);
+        }
+
+        private static SqlContext GetSqlContext()
+        {
+            return sqlContext ?? (sqlContext = new SqlContext(SparkContext));
+        }
+    }
+}
