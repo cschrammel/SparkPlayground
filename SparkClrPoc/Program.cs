722dc1250e993eb137fc0cbaee29f964b02034c3
unknown <cschrammel@MIT-L-U17917.us.rwbaird.com>
2016-02-03 12:30:29 -0600

    Got something working!

--- SparkClrPoc/Program.cs
+++ SparkClrPoc/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
 using System.IO;
-using System.Text.RegularExpressions;
+using System.Linq;
 using Microsoft.Spark.CSharp.Core;
 using Microsoft.Spark.CSharp.Sql;
 
@@ -13,7 +9,7 @@ namespace SparkClrPoc
 {
     class Program
     {
-        private static SqlContext sqlContext;
+        private static SqlContext _sqlContext;
         internal static SparkContext SparkContext;
 
         static void Main(string[] args)
@@ -21,21 +17,52 @@ namespace SparkClrPoc
             SparkContext = CreateSparkContext();
             SparkContext.SetCheckpointDir(Path.GetTempPath());
 
-            var crimeDataFrame = GetSqlContext().Read().Json(@"data\SacramentocrimeJanuary2006.csv");
+            var schema = new StructType(new List<StructField>
+            {
+                new StructField("cdatetime", new StringType(), true),
+                new StructField("address", new StringType(), true),
+                new StructField("district", new StringType(), true),
+                new StructField("beat", new StringType(), true),
+                new StructField("grid", new StringType(), true),
+                new StructField("crimedescr", new StringType(), true),
+                new StructField("ucr_ncic_code", new StringType(), true),
+                new StructField("latitude", new StringType(), true),
+                new StructField("longitude", new StringType(), true),
+            });
 
-            SparkContext.Stop();
+            var crimeDataFrame = GetSqlContext()
+                .TextFile(@"C:\github\sparkplayground\SparkClrPoc\Data\SacramentocrimeJanuary2006.csv",
+                    schema);
+
+            //var row = crimeDataFrame.First();
+            //var schema = row.GetSchema();
+
+            crimeDataFrame.RegisterTempTable("crime");
+
+            var sqlResults = GetSqlContext().Sql("SELECT crimedescr, COUNT(*) AS Count " +
+                                                 "FROM crime " +
+                                                 "WHERE crimedescr LIKE '%THEFT%' " +
+                                                 "GROUP BY crimedescr " +
+                                                 "ORDER BY 2 DESC ");
+            sqlResults
+                .Select("crimedescr", "Count")
+                .Collect()
+                .ToList()
+                .ForEach(Console.WriteLine);
+            
+            //SparkContext.Stop();
         }
 
-        // Creates and returns a context
         private static SparkContext CreateSparkContext()
         {
-            var conf = new SparkConf();
-            return new SparkContext(conf);
+            //var conf = new SparkConf();
+            //conf.Set("spark.local.dir", @"C:\temp");
+            return new SparkContext("local", "sparkSqlTest");
         }
 
         private static SqlContext GetSqlContext()
         {
-            return sqlContext ?? (sqlContext = new SqlContext(SparkContext));
+            return _sqlContext ?? (_sqlContext = new SqlContext(SparkContext));
         }
     }
 }
