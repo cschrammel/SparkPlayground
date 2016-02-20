4bdc3cf66e32da9403422cc21962b503192e08c2
unknown <carl.schrammel@gmail.com>
2016-02-20 08:07:21 -0600

    Parse with type conversion

--- SparkClrPoc/FixedWidthController.cs
+++ SparkClrPoc/FixedWidthController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using Microsoft.Spark.CSharp.Core;
 using Microsoft.Spark.CSharp.Sql;
-using SparkClrPoc.Models;
 
 namespace SparkClrPoc
 {
@@ -19,30 +17,51 @@ namespace SparkClrPoc
          
         }
 
-        public IEnumerable<string> Get()
+        public IEnumerable<Model> Get()
         {
             _sparkContext = Program.SparkContext;
             var crimeDataFrame = GetSqlContext()
                 .TextFile(CrimeFilePath)
-                .Map(l=>((string)l.Get(0)).Substring(0,1))
                 .Cache();
 
-            var tempTdd = _sparkContext.TextFile(CrimeFilePath).Map(l => new object[] { l.Substring(0, 1) });
+            var tempTdd = _sparkContext.TextFile(CrimeFilePath)
+                .Map(l => new object[]
+                        {
+                            int.Parse(l.Substring(0, 3)),
+                            int.Parse(l.Substring(4, 3)),
+                            int.Parse(l.Substring(8, 4)),
+                        });
 
             var data = GetSqlContext().CreateDataFrame(tempTdd, new StructType(new List<StructField>
                         {
-                            new StructField("Line", new StringType())
+                            new StructField("Field1", new IntegerType()),
+                            new StructField("Field2", new IntegerType()),
+                            new StructField("Field3", new IntegerType())
                         }));
 
             data.Show();
-            return data.Collect().Select(l=>l.ToString()).ToList();
-
-            return new List<string>();
+            data.RegisterTempTable("data");
+            
+            return GetSqlContext().Sql("SELECT Field1, Field2, Field3 FROM data")
+                .Collect()
+                .Select(l => new Model
+                {
+                    Field1 = l.Get("Field1"),
+                    Field2 = l.Get("Field2"),
+                    Field3 = l.Get("Field3"),
+                }).ToList();
         }
 
         private static SqlContext GetSqlContext()
         {
             return _sqlContext ?? (_sqlContext = new SqlContext(_sparkContext));
         }
+    }
+
+    public class Model
+    {
+        public int Field1 { get; set; }
+        public int Field2 { get; set; }
+        public int Field3 { get; set; }
     }
 }
